5d4d5b09c83c3e1bf0db3dcfb91709db524ad003
William Towe <willbur1984@gmail.com>
2018-03-03 16:25:49 -0500

    add KSOChatKit target

---
+++ KSOChatKit/KSOChatKit.h
@@ -0,0 +1,19 @@
+//
+//  KSOChatKit.h
+//  KSOChatKit
+//
+//  Created by William Towe on 3/3/18.
+//  Copyright © 2018 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSOChatKit.
+FOUNDATION_EXPORT double KSOChatKitVersionNumber;
+
+//! Project version string for KSOChatKit.
+FOUNDATION_EXPORT const unsigned char KSOChatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSOChatKit/PublicHeader.h>
+
+
